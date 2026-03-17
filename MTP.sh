3a67ad29bd126629ba675c5bc62a6ca2f598d580
alnawei <122384898+alnawei@users.noreply.github.com>
2026-03-17 16:52:28 +0800

    Change default FAKE_TLS_DOMAIN to icloud.com

--- MTP.sh
+++ MTP.sh
@@ -92,7 +92,7 @@ get_mtg_config() {
     
     if [ "$service_type" = "faketls" ]; then
         read -p "请输入用于伪装的域名 (默认 azure.microsoft.com): " FAKE_TLS_DOMAIN
-        if [ -z "$FAKE_TLS_DOMAIN" ]; then FAKE_TLS_DOMAIN="azure.microsoft.com"; fi
+        if [ -z "$FAKE_TLS_DOMAIN" ]; then FAKE_TLS_DOMAIN="icloud.com"; fi
         SECRET=$("$BIN_PATH" generate-secret --hex "$FAKE_TLS_DOMAIN")
     else
         SECRET=$("$BIN_PATH" generate-secret "secured")
